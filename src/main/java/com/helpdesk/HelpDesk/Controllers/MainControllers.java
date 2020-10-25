6f3ba7ea97bb949799178e8735a809f312a93a1b
Santiago Vargas Avendano <savargas@unal.edu.co>
2020-10-24 20:29:52 -0500

    Crear la vista de mis solicitudes agente y quitar el .html de los login de cerrar sesión

--- src/main/java/com/helpdesk/HelpDesk/Controllers/MainControllers.java
+++ src/main/java/com/helpdesk/HelpDesk/Controllers/MainControllers.java
@@ -17,6 +17,11 @@ public class MainControllers {
 
     // Vistas del Login
 
+    @RequestMapping("/login")
+    public String login(){
+        return "redirect:/";
+    }
+
     @GetMapping("/")
     public String loginDefault(Model model){
         model.addAttribute("loginForm", new LoginForm());
@@ -27,10 +32,10 @@ public class MainControllers {
     public String loginPost(@ModelAttribute LoginForm form, Model model){
         System.out.println(form.getUserName()); //tests
         System.out.println(form.getPassword());
-        if(form.getUserName().equals("savargas")){
+        if(form.getUserName().equals("savargas@unal.edu.co")){
             return "redirect:/create-request-user";
-        }else if(form.getUserName().equals("rcalvom")){
-            // return "redirect:/requestAgent";
+        }else if(form.getUserName().equals("rcalvom@unal.edu.co")){
+             return "redirect:/my-requests-agent";
         }// Es necesario hacer acá el login con respecto a la BD
         return "login";
     }
@@ -45,21 +50,42 @@ public class MainControllers {
     }
     @PostMapping("/create-request-user")
     public String createRequestUserPost(@ModelAttribute CreateRequestForm form, Model model){
+        System.out.println(form.getDescription());
+        // Request request = new Request(form.getDescription());
         return "redirect:/my-requests-user";
     }
 
     //Mis solicitudes
     @GetMapping("/my-requests-user")
     public String myRequestsUserDefault(Model model){
-        /* List<car> list = new ArrayList<car>();
-        list.add(new car("IWR","Rojo"));
-        list.add(new car("IRS","Azul"));
-        Lista con las solicitudes de la base de datos filtrada por el usuario
+        /* List<Request> requests = new ArrayList<Request>();
+        requests = lista con las solicitudes de la base de datos filtrada por el usuario
         model.addAttribute("Requests", list);*/
         return "my-requests-user";
     }
     @PostMapping("/my-requests-user")
     public String myRequestsUserPost(Model model){
         return "my-requests-user";
+    }
+
+    @RequestMapping("/request-details/{ID?}")
+    public String a(){
+        return "request-details";
+    }
+
+    // Controladores del Agente
+
+    //Mis solicitudes
+    @GetMapping("/my-requests-agent")
+    public String myRequestsAgentDefault(Model model){
+        /* List<Request> requests = new ArrayList<Request>();
+        requests = lista con las solicitudes de la base de datos filtrada por el usuario
+
+        model.addAttribute("Requests", list);*/
+        return "my-requests-agent";
+    }
+    @PostMapping("/my-requests-agent")
+    public String myRequestsAgentPost(Model model){
+        return "my-requests-agent";
     }
 }
