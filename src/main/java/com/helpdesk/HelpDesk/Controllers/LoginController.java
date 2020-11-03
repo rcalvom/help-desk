3f19e3fa19acfaabc39f75fba11d19b5d5774f7f
Santiago Vargas Avendano <savargas@unal.edu.co>
2020-11-02 20:10:18 -0500

    Formulario para llenar los datos del inicio de sesión, puede ser inutil

--- src/main/java/com/helpdesk/HelpDesk/Controllers/LoginController.java
+++ src/main/java/com/helpdesk/HelpDesk/Controllers/LoginController.java
@@ -1,12 +1,27 @@
 package com.helpdesk.HelpDesk.Controllers;
 
+import com.helpdesk.HelpDesk.DAO.BoundingTypeDAO;
+import com.helpdesk.HelpDesk.DAO.DependencyDAO;
+import com.helpdesk.HelpDesk.DAO.RequestDAO;
+import com.helpdesk.HelpDesk.Forms.DataLogginForm;
 import com.helpdesk.HelpDesk.Forms.LoginForm;
+import com.helpdesk.HelpDesk.Models.BoundingType;
+import com.helpdesk.HelpDesk.Models.Dependency;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class LoginController {
+
+    @Autowired
+    private BoundingTypeDAO boundingTypeDAO;
+
+    @Autowired
+    private DependencyDAO dependencyDAO;
 
     @GetMapping({"/", "/login"})
     public String loginDefault(Model model){
@@ -25,5 +40,22 @@ public class LoginController {
                 return "redirect:/admin/inbox";
         } // TODO: Conectar con la base de datos para el login
         return "login";
+    }
+
+    @GetMapping("/data-login")
+    public String dataLoggingDefault(Model model){
+        DataLogginForm form = new DataLogginForm();
+        model.addAttribute("dataLogging", form);
+        List<BoundingType> boundingTypeList = (List<BoundingType>) boundingTypeDAO.select();
+        model.addAttribute("boundingTypes", boundingTypeList);
+        List<Dependency> dependencies = (List<Dependency>) dependencyDAO.select();
+        model.addAttribute("dependencies", dependencies);
+        return "data-logging";
+    }
+
+    @PostMapping("/data-login")
+    public String dataLoggingPost(@ModelAttribute DataLogginForm form){
+        // TODO: Registrar los datos del usuario
+        return "redirect:/data-login";
     }
 }
---
+++ src/main/java/com/helpdesk/HelpDesk/Forms/DataLogginForm.java
@@ -0,0 +1,22 @@
+package com.helpdesk.HelpDesk.Forms;
+
+public class DataLogginForm {
+    private String boundingType;
+    private String dependency;
+
+    public String getBoundingType() {
+        return boundingType;
+    }
+
+    public void setBoundingType(String boundingType) {
+        this.boundingType = boundingType;
+    }
+
+    public String getDependency() {
+        return dependency;
+    }
+
+    public void setDependency(String dependency) {
+        this.dependency = dependency;
+    }
+}
