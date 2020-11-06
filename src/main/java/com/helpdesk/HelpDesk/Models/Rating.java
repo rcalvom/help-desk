9fef07cfb5dc4882dc0ac708a256c701fbc0b66e
Samuel Gonzalez <sagonzalezn@unal.edu.co>
2020-11-05 21:28:50 -0500

    Fix Rating

---
+++ src/main/java/com/helpdesk/HelpDesk/Models/Rating.java
@@ -0,0 +1,23 @@
+package com.helpdesk.HelpDesk.Models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Rating {
+
+    @Id
+    @Size(max = 25)
+    private String name;
+
+    public Rating(){}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ src/main/java/com/helpdesk/HelpDesk/Repository/RatingRepository.java
@@ -0,0 +1,8 @@
+package com.helpdesk.HelpDesk.Repository;
+
+import com.helpdesk.HelpDesk.Models.Rating;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RatingRepository extends CrudRepository<Rating, String> {
+
+}
