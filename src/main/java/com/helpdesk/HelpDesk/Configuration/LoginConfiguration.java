25ff365ef1385e4fdf1dd78665ebbc08cf740952
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2020-11-06 15:33:02 -0500

    Login con google funcional, pendiente configuración de roles y arreglos en las vistas

---
+++ src/main/java/com/helpdesk/HelpDesk/Configuration/LoginConfiguration.java
@@ -0,0 +1,21 @@
+package com.helpdesk.HelpDesk.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class LoginConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login", "/css/**", "/js/**", "/images/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login()
+                .loginPage("/login");
+
+    }
+}
--- src/main/java/com/helpdesk/HelpDesk/Controllers/LoginController.java
+++ src/main/java/com/helpdesk/HelpDesk/Controllers/LoginController.java
@@ -2,17 +2,21 @@ package com.helpdesk.HelpDesk.Controllers;
 
 import com.helpdesk.HelpDesk.DAO.BoundingTypeDAO;
 import com.helpdesk.HelpDesk.DAO.DependencyDAO;
-import com.helpdesk.HelpDesk.DAO.RequestDAO;
 import com.helpdesk.HelpDesk.Forms.DataLogginForm;
 import com.helpdesk.HelpDesk.Forms.LoginForm;
 import com.helpdesk.HelpDesk.Models.BoundingType;
 import com.helpdesk.HelpDesk.Models.Dependency;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -23,13 +27,33 @@ public class LoginController {
     @Autowired
     private DependencyDAO dependencyDAO;
 
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    private static final String authorizationRequestBaseUri = "oauth2/authorization";
+    private final Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+
+
     @GetMapping({"/", "/login"})
     public String loginDefault(Model model){
+        Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
+                .as(Iterable.class);
+        if (type != ResolvableType.NONE &&
+                ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        }
+
+        assert clientRegistrations != null;
+        clientRegistrations.forEach(registration ->
+                oauth2AuthenticationUrls.put(registration.getClientName(),
+                        authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+        model.addAttribute("urls", oauth2AuthenticationUrls);
         model.addAttribute("loginForm", new LoginForm());
         return "login";
     }
 
-    @PostMapping({"/", "/login"})
+    /*@PostMapping({"/", "/login"})
     public String loginPost(@ModelAttribute LoginForm form){
         switch (form.getUsername()) {
             case "user":
@@ -40,7 +64,7 @@ public class LoginController {
                 return "redirect:/admin/inbox";
         } // TODO: Conectar con la base de datos para el login
         return "login";
-    }
+    }*/
 
     @GetMapping("/data-login")
     public String dataLoggingDefault(Model model){
